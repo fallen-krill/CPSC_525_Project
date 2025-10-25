b3d41857d2ed8c3b141081aba4cdf5727dbc0178
krill <vrtx65535@protonmail.com>
2025-10-25 15:12:01 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CPSC 525 Project
+## CWE 502: Deserialization of Untrusted data
+
+## Coding portion
---
+++ main.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+
+def main():
+    t = Turtle()
+    t.screen.mainloop()
+
+main()
