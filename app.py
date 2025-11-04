7475445a04c3df2761251a78660571d9514b4430
TypicalAlias <vanja@anm.org>
2025-11-03 22:16:30 -0700

    new equation added when last one filled out

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 import sys
+from PySide6.QtCore import (
+    QPoint, QRect, Slot
+)
+from PySide6.QtGui import (
+    QPixmap, QTransform
+)
 from PySide6.QtWidgets import (
     QWidget, QMainWindow, QApplication, QHBoxLayout, QSplitter, QTableWidget, 
-    QHeaderView, QGraphicsView, QGraphicsScene
+    QTableWidgetItem, QHeaderView, QGraphicsView, QGraphicsScene
     )
 
 class Widget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.table = QTableWidget(1, 1, self)
+        self.table = QTableWidget(2, 1, self)
         self.table.setHorizontalHeaderLabels(["Equation"])
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         self.scene = QGraphicsScene()
+        self.img = QPixmap("image.JPG")
+
         self.graph = QGraphicsView(self.scene)
+        rect = self.graph.viewport().geometry()
+        self.img = self.img.scaledToHeight(rect.height())
+        self.img = self.img.scaledToWidth(rect.width())
+        self.scene.addPixmap(self.img)
 
         # splitter layout
         self.splitter = QSplitter(self)
@@ -26,6 +38,14 @@ class Widget(QWidget):
         # overall layout
         self.layout = QHBoxLayout(self)
         self.layout.addWidget(self.splitter)
+
+        self.table.itemChanged.connect(self.add_equation)
+
+    @Slot()
+    def add_equation(self, item: QTableWidgetItem):
+        last_row = self.table.rowCount() - 1
+        if (item.row() == last_row):
+            self.table.insertRow(last_row + 1)
 
 class MainWindow(QMainWindow):
     def __init__(self, widget):
