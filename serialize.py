9107ac8ca2e59fb81e552e2dd1208bb2a815e3d0
elongated-dog <jakewaiand@shaw.ca>
2025-11-25 22:49:39 -0700

    experimenting with possible exploits
    - contains the widgets gallery example from the qt documentation, i used it for testing ability to open windows through pickle.

--- serialize.py
+++ serialize.py
@@ -1,19 +1,16 @@
 import pickle
 
-# everything here is temporary or a placeholder
-# both serializing and deserializing need to deal with a data structure
-# deserializing: will have to take input and properly construct said data structure
-
 def save(fname, data):
-    """serializes data given to it and writes to a file
-    returns 1 if write was successful
-    returns 0 otherwise"""
+    """Serializes given data and saves it to a file given by fname as a path.
+    The file will be overwritten if it already exists. Otherwise, a new file
+    is created.
+    Returns 1 if write was successful
+    Returns 0 otherwise"""
     serialized_data = pickle.dumps(data)
 
     try:
         with open(fname, "wb") as f:
             f.write(serialized_data)
-        #print(f"Wrote data to {fname}")
 
     except OSError as e:
         return 0
@@ -21,17 +18,14 @@ def save(fname, data):
     return 1
 
 def load(fname):
-    """deserializes data read from a file
-    returns deserialized data if read was successful
-    returns None otherwise"""
+    """Deserializes data from file, given by fname as a path.
+    Returns deserialized data if read was successful
+    Returns None otherwise"""
     try:
         with open(fname, "rb") as f:
             serialized_data = f.read()
 
         deserialized_data = pickle.loads(serialized_data, encoding="ASCII")
-
-        #print(f"Loaded data from {fname};")
-        #print(deserialized_data)
 
     except OSError as e:
         print(f"Cannot open file; {e}")
